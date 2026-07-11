8a4f2dc43af3a0916490af4ef1a3fd45902b6b1c
Soumajit Chakraborty <156880318+soumajit67@users.noreply.github.com>
2026-07-11 09:00:56 +0530

    Update AsyncFS.js

--- File System/AsyncFS.js
+++ File System/AsyncFS.js
@@ -1,6 +1,7 @@
 // Import the file system module
 const fs = require("fs");
 
+
 // Write a file
 fs.writeFile("sample.txt", "Hello Node.js!", (err) => {
     if (err) {
@@ -10,6 +11,8 @@ fs.writeFile("sample.txt", "Hello Node.js!", (err) => {
 
     console.log("File written successfully.");
 
+
+    
     // Read the file
     fs.readFile("sample.txt", "utf8", (err, data) => {
         if (err) {
@@ -19,4 +22,27 @@ fs.writeFile("sample.txt", "Hello Node.js!", (err) => {
 
         console.log(data);
 
+        
+  // Update the file
+        fs.appendFile("sample.txt", "\nWelcome to Express.", (err) => {
+            if (err) {
+                console.log("Error updating file:", err);
+                return;
+            }
+
+            console.log("File updated successfully.");
+
+            
+            // Delete the file
+            fs.unlink("sample.txt", (err) => {
+                if (err) {
+                    console.log("Error deleting file:", err);
+                    return;
+                }
+
+                console.log("File deleted successfully.");
+            });
+        });
+    });
+});
       
